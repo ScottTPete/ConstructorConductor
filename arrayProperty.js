230be9773b9ce0914c488584cdb53ded0d545fda
Scott Peterson <stpeterson28@yahoo.com>
2016-02-10 21:57:12 -0700

    Finished the constructor conductor project

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,21 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
-  //code here
+
+
+
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+//
+// }
+//
+//
+// reverseString("Hello");
+
+String.prototype.reverse = function() {
+  return this.split('').reverse().join('');
+}
+
+var wor = "hi"
+
+console.log(wor.reverse());
--- sayName.js
+++ sayName.js
@@ -1,13 +1,25 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
-  //code here
+var Person = function(name, age) {
+  this.name = name;
+  this.age = age;
+//   this.sayName = function() {
+//     console.log(this.name);
+//   }
+}
 
 
 //Now create three instances of Person with data you make up
 
-  //code here
+var bob = new Person("Bob", 67);
+var batman = new Person("Batman", 35);
+var ichiro = new Person("Ichiro", 39);
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
-  //code here
+Person.prototype.sayName = function() {
+  alert(this.name);
+}
+
+batman.sayName();
